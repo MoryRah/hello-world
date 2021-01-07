122bd620c6ae22055a927fe887dd345a2a227ff6
MoryRah <71174344+MoryRah@users.noreply.github.com>
2021-01-07 17:52:27 +0330

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,12 @@
-package com.mycompany.app;
-
 /**
  * Hello world!
  */
-    class HelloWorld{  
-        public static void main(String args[]){  
-         System.out.println("Hello Java");  
-        }  
-    }  
+   class HelloWorld 
+{ 
+    // Your program begins with a call to main(). 
+    // Prints "Hello, World" to the terminal window. 
+    public static void main(String args[]) 
+    { 
+        System.out.println("Hello, World"); 
+    } 
+} 
